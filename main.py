eae9e52722cde1412cc7fa81de96281c390f1302
GabrielRandolfo <0001140004@senaimgaluno.com.br>
2026-03-05 20:29:38 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+from models.menu import Menu
+from models.paciente import Paciente
+from models.classificador import Classificador
+from models.triagem import Triagem
+from models.gerador_senha import GeradorSenha
+from models.impressora import Impressora
+
+
+def main():
+
+    menu = Menu()
+    classificador = Classificador()
+    triagem = Triagem()
+    gerador = GeradorSenha()
+
+    while True:
+
+        print("\n=== TOTEM DE ATENDIMENTO ===")
+
+        nome = input("Nome do paciente: ")
+        idade = int(input("Idade: "))
+        pcd_input = input("Paciente PCD? (s/n): ")
+
+        pcd = pcd_input.lower() == "s"
+
+        paciente = Paciente(nome, idade, pcd)
+
+        # abre o menu
+        servico = menu.mostrar_menu()
+
+        # verifica emergência
+        alerta = triagem.avaliar(servico)
+
+        if alerta:
+            print("\n⚠️", alerta)
+            continue
+
+        # classifica prioridade
+        prioridade = classificador.classificar(paciente)
+
+        # gera senha
+        senha = gerador.gerar(prioridade)
+
+        print("\n=== TICKET ===")
+        print("Paciente:", paciente.nome)
+        print("Serviço:", servico)
+        print("Prioridade:", prioridade)
+        print("Senha:", senha)
+
+        print("\nRetornando ao menu...\n")
+
+
+if __name__ == "__main__":
+    main()
